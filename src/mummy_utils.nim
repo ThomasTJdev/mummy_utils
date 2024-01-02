2972fc42f71b5cde1e8ab096e2407b480d64115c
ThomasTJdev <ttj@ttj.dk>
2024-01-02 13:54:34 +0100

    Use headers if they are declared in resp (issue #1 closed)

--- src/mummy_utils.nim
+++ src/mummy_utils.nim
@@ -380,16 +380,36 @@ template routerSet*(
 # General response
 #
 template resp*(
+    httpStatus: HttpCode = Http200,
+    headers: HttpHeaders,
     body: string
   ) =
-  request.respond(200, @[("Content-Type", $ContentType.Html)], body)
+  when defined(dev):
+    if headers["Content-Type"] == "":
+      echo "Warning: No Content-Type set in response, defaulting to " & $ContentType.Html
+
+  request.respond(httpStatus.ord, headers, body)
+  return
+
+
+template resp*(
+    body: string,
+    contentType = $ContentType.Html
+  ) =
+  ## Returns with text/html as default.
+  when declared(headers):
+    setHeader("Content-Type", $contentType)
+    request.respond(200, headers, body)
+  else:
+    request.respond(200, @[("Content-Type", contentType)], body)
   return
 
 
 template resp*(
     httpStatus: HttpCode,
   ) =
-  request.respond(httpStatus.ord, @[("Content-Type", $ContentType.Html)], "")
+  ## Returns response code with empty headers and body.
+  request.respond(httpStatus.ord, emptyHttpHeaders(), "")
   return
 
 
@@ -397,7 +417,12 @@ template resp*(
     httpStatus: HttpCode,
     body: string
   ) =
-  request.respond(httpStatus.ord, @[("Content-Type", $ContentType.Html)], body)
+  when declared(headers):
+    if not headers.contains("Content-Type"):
+      setHeader("Content-Type", $ContentType.Html)
+    request.respond(httpStatus.ord, headers, body)
+  else:
+    request.respond(httpStatus.ord, @[("Content-Type", $ContentType.Html)], body)
   return
 
 
@@ -406,37 +431,36 @@ template resp*(
     contentType: ContentType,
     body: string
   ) =
-  request.respond(httpStatus.ord, @[("Content-Type", $contentType)], body)
+  when declared(headers):
+    setHeader("Content-Type", $contentType)
+    request.respond(httpStatus.ord, headers, body)
+  else:
+    request.respond(httpStatus.ord, @[("Content-Type", $contentType)], body)
   return
 
-
-template resp*(
-    httpStatus: HttpCode = Http200,
-    headers: HttpHeaders,
-    body: string
-  ) =
-  if headers["Content-Type"] == "":
-    headers["Content-Type"] = $ContentType.Html
-    when defined(dev):
-      echo "Warning: No Content-Type set in response, defaulting to " & $ContentType.Html
-  request.respond(httpStatus.ord, headers, body)
-  return
 
 
 #
 # Json response
 #
 template resp*(body: JsonNode) =
-  request.respond(200, @[("Content-Type", $ContentType.Json)], $body)
+  when declared(headers):
+    setHeader("Content-Type", $ContentType.Json)
+    request.respond(200, headers, $body)
+  else:
+    request.respond(200, @[("Content-Type", $ContentType.Json)], $body)
   return
+
+
+template resp*(httpStatus: HttpCode, contentType: ContentType, body: JsonNode) =
+  request.respond(httpStatus.ord, @[("Content-Type", $contentType)], $body)
+  return
+
 
 template resp*(httpStatus: HttpCode, body: JsonNode) =
   request.respond(httpStatus.ord, @[("Content-Type", $ContentType.Json)], $body)
   return
 
-template resp*(httpStatus: HttpCode, contentType: ContentType, body: JsonNode) =
-  request.respond(httpStatus.ord, @[("Content-Type", $contentType)], $body)
-  return
 
 
 #
@@ -456,11 +480,24 @@ template sendFile*(path: string) =
 # Redirects
 #
 template redirect*(path: string) =
-  request.respond(303, @[("Location", path)])
+  when declared(headers):
+    headers["Location"] = path
+    request.respond(303, headers)
+  else:
+    request.respond(303, @[("Location", path)])
   return
 
 template redirect*(httpStatus: HttpCode, path: string) =
-  request.respond(httpStatus.ord, @[("Location", path)])
+  when declared(headers):
+    headers["Location"] = path
+    request.respond(httpStatus.ord, headers)
+  else:
+    request.respond(httpStatus.ord, @[("Location", path)])
+  return
+
+template redirect*(httpStatus: HttpCode, headers: HttpHeaders, path: string) =
+  headers["Location"] = path
+  request.respond(httpStatus.ord, headers)
   return
 
 
